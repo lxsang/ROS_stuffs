ac11bc4c0dc4012c4eef13df9f1cfa436be1c5e5
lxsang <xsang.le@gmail.com>
2017-08-10 16:57:04 +0200

    robot_discover service now provides a list of neighbours

--- src/multi_master_bridge/src/bridge/robot_discover.cpp
+++ src/multi_master_bridge/src/bridge/robot_discover.cpp
@@ -11,8 +11,9 @@ extern "C"
 #endif
  #include <signal.h>
 #include "multi_master_bridge/NeighbourId.h"
+#include "multi_master_bridge/NeighbourList.h"
 static int sockfd = -1;
-
+int robot_decay_time_s;
 
 void sig_handle(int s)
 {
@@ -23,19 +24,24 @@ void sig_handle(int s)
 }
 int listen_to;
 std::string listen_interface;
+multi_master_bridge::NeighbourList mylist;
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "watch_dog",ros::init_options::NoSigintHandler);
 	ros::NodeHandle n("~");
 	n.param<int>("listen_to", listen_to, 9191);
     n.param<std::string>("listen_interface",listen_interface, "wlan0");
+	n.param<int>("robot_decay_time_s",robot_decay_time_s, 5);
 	signal(SIGINT, sig_handle);
-	
-	ros::Publisher dog_pub = n.advertise<multi_master_bridge::NeighbourId>("/new_robot", 1000);
+	mylist.size = 0;
+	ros::Publisher dog_pub = n.advertise<multi_master_bridge::NeighbourList>("/new_robot", 1000);
 	ros::Rate loop_rate(10);
 
 	sockfd = bind_udp_socket(listen_to);
 	struct inet_id_ id = read_inet_id(listen_interface.c_str());
+	bool robot_found = false;
+	int found_index = -1;
+	multi_master_bridge::NeighbourId msg;
 	if(sockfd > 0)
 	{
 		while (ros::ok())
@@ -43,20 +49,47 @@ int main(int argc, char **argv)
 			struct beacon_t a = sniff_beacon(sockfd,id);
 			if(a.status)
 			{
-				multi_master_bridge::NeighbourId msg;
-				std::stringstream ss;
+				// check if the id is exist
 				msg.header.stamp = ros::Time::now();
 				msg.header.frame_id = std::string(a.hostname);
-				msg.ip = inet_ntoa(a.ip);
+				msg.ip = std::string(inet_ntoa(a.ip));
 				msg.name = std::string(a.hostname);
 				msg.port = a.port;
-				ss << " Found neighbour at ";
-				ss << msg.ip;
-				ss << " name " <<msg.name;
-				ss << " listen on:" << msg.port;
-				//msg.data = ss.str();
-				ROS_INFO("%s", ss.str().c_str());
-				dog_pub.publish(msg);
+				robot_found = true;
+				//mylist.push_back(msg);
+			}
+			ros::Time t = ros::Time::now();
+			for(int i = 0; i < mylist.size;i++)
+			{
+				// check if decay
+				if(t.sec -  mylist.list[i].header.stamp.nsec > robot_decay_time_s)
+				{
+					mylist.list[i].status = -1; // ofline
+					ROS_INFO(" %s become offline, it is no longer available for exchanging", mylist.list[i].name.c_str());
+				}
+				if(found_index != -1 && robot_found && msg.ip.compare(mylist.list[i].ip) == 0)
+				{
+					found_index = i;
+				}
+			}
+			if(robot_found)
+			{
+				if(found_index != -1)
+				{
+					ROS_INFO("neighbour is existing, update its info");
+					mylist.list[found_index].header = msg.header;
+					mylist.list[found_index].name = msg.name;
+					mylist.list[found_index].port = msg.port;
+					mylist.list[found_index].status =0;
+					found_index = -1;
+				} else 
+				{
+					mylist.size++;
+					msg.status = 1;
+					mylist.list.push_back(msg);
+				}
+				dog_pub.publish(mylist);
+				robot_found = false;
 			}
 			ros::spinOnce();
 			loop_rate.sleep();
--- src/multi_master_bridge/src/map_feeding.cpp
+++ src/multi_master_bridge/src/map_feeding.cpp
@@ -11,20 +11,19 @@ extern "C"
 #endif
 #include "helpers/helper.h"
 #include "multi_master_bridge/NeighbourId.h"
+#include "multi_master_bridge/NeighbourList.h"
 #define EXPIRED_TIME 5 //s
 
 ros::Publisher pub;
-std::map<string,multi_master_bridge::NeighbourId*> neighbors;
-std::string publish_to,_interface,map_update_;
+multi_master_bridge::NeighbourList neighbors;
+std::string publish_to,_interface,map_update_,map_local_;
 double _init_x,_init_y,_init_z,_sending_rate;
 static int sockfd=-1;
-int robot_decay_time_s;
 struct inet_id_ id;
 
-void neighbors_discover(const multi_master_bridge::NeighbourId::ConstPtr& msg)
-{ 
-	neighbors[msg->ip] = new multi_master_bridge::NeighbourId(*msg);
-	// remove all unactive neighbours
+void neighbors_discover(const multi_master_bridge::NeighbourList::ConstPtr& msg)
+{
+	neighbors = *msg;
 }
 void send_newmap(const multi_master_bridge::MapData::ConstPtr& msg)
 {
@@ -42,11 +41,13 @@ void send_newmap(const multi_master_bridge::MapData::ConstPtr& msg)
 	d.publish_to = (char*)publish_to.c_str();
 	d.hash = MapDataHelper::hash();
 	//send data to all neighbour
-	for(std::map<string,multi_master_bridge::NeighbourId*>::iterator it = neighbors.begin(); it != neighbors.end(); it++)
+	for(int i = 0; i < neighbors.size;i++ )
 	{
-		ROS_INFO("Feed map to %s:%d",it->second->ip.c_str(),it->second->port);
-		teleport_raw_data(it->second->ip.c_str(),it->second->port,d);
-
+		if(neighbors.list[i].status != -1)
+		{
+			ROS_INFO("Feed map to %s (%s):%d", neighbors.list[i].ip.c_str(),  neighbors.list[i].name.c_str() , neighbors.list[i].port);
+			teleport_raw_data( neighbors.list[i].ip.c_str(), neighbors.list[i].port,d);
+		}
 		// free allocated memory
 		//ROS_INFO("Feed x:%f, st.sec:%d, st.nsec:%d, fid:%s", m.seq, m.stamp.sec,m.stamp.nsec,m.frame_id.c_str());
 		//pub.publish(m);
@@ -60,30 +61,22 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "map_exchange");
 	ros::NodeHandle n("~");
 	n.param<std::string>("publish_to",publish_to, "/other_map");
-	n.param<int>("robot_decay_time_s",robot_decay_time_s, 5);
 	n.param<std::string>("network_interface",_interface, "wlan0");
-	 n.param<std::string>("map_update_topic",map_update_, "/map_update");
+	n.param<std::string>("map_update_topic",map_update_, "/map_update");
+	n.param<std::string>("my_local_map",map_local_, "/map");
 	n.param<double>("init_z",_init_z, 0.0);
 	n.param<double>("init_x",_init_x, 0.0);
 	n.param<double>("init_y",_init_y, 0.0);
 	n.param<double>("sending_rate",_sending_rate, 1.0);
+	neighbors.size = 0;
 	//pub = n.advertise<nav_msgs::OccupancyGrid>("other_map", 1000);
-	ros::Subscriber sub = n.subscribe<multi_master_bridge::NeighbourId>("/new_robot", 50,&neighbors_discover);
+	ros::Subscriber sub = n.subscribe<multi_master_bridge::NeighbourList>("/new_robot", 50,&neighbors_discover);
 	ros::Subscriber sub1 = n.subscribe<multi_master_bridge::MapData>(map_update_, 100,&send_newmap);
 	ros::Rate loop_rate(_sending_rate);
 	id = read_inet_id(_interface.c_str());
 	ROS_INFO("My address %s on %s",inet_ntoa(id.ip), _interface.c_str());
 	while(ros::ok())
 	{
-		ros::Time t = ros::Time::now();
-		for(std::map<string,multi_master_bridge::NeighbourId*>::iterator it = neighbors.begin(); it != neighbors.end(); it++)
-		{
-			if(t.sec - it->second->header.stamp.sec > robot_decay_time_s)
-			{
-				ROS_INFO("Remove %s from neighbours",it->second->name.c_str());
-				neighbors.erase(it);
-			} 
-		}
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
