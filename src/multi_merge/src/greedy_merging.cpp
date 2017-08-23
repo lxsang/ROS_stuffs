08337b26f22728e8ee77319a2399a40cf81441d8
lxsang <xsang.le@gmail.com>
2017-08-23 12:32:00 +0200

    add dependencies files

--- src/multi_merge/src/greedy_merging.cpp
+++ src/multi_merge/src/greedy_merging.cpp
@@ -4,15 +4,15 @@
 #include "geometry_msgs/Pose.h"
 
 #define UNKNOWN -1
-
-std::string other_map_,my_map_,merged_map_topic,map_update_,map_other_;
-nav_msgs::OccupancyGridPtr global_map;
-bool greedy_local_merge;
+std::string other_map_,my_map_,merged_map_topic,map_update_;
+bool furious_merge, has_local;
+nav_msgs::OccupancyGrid global_map;
+nav_msgs::OccupancyGridPtr local_map;
 geometry_msgs::Pose my_pose;
 ros::Publisher  global_map_pub;
-ros::Publisher  other_map_pub;
 ros::Publisher  map_update_pub;
-
+std::map<std::string,  multi_master_bridge::MapData> pipeline;
+double map_width_m_, map_height_m_, map_resolution_;
 void getRelativePose(geometry_msgs::Pose p, geometry_msgs::Pose q, geometry_msgs::Pose &delta, double resolution) {
   
     delta.position.x = round((p.position.x - q.position.x) / resolution);
@@ -45,75 +45,74 @@ void greedyMerging(int delta_x, int delta_y, const nav_msgs::OccupancyGrid their
     }
   }
 }*/
-void greedyMerging(int delta_x, int delta_y, int x, int y, const nav_msgs::OccupancyGrid their_map, bool local) {
+void greedyMerging(int delta_x, int delta_y, int x, int y, const nav_msgs::OccupancyGrid their_map, bool furious) {
   for(int i = 0; i < (int)their_map.info.width; i++) {
     for(int j = 0; j < (int)their_map.info.height; j++) {
-      if(i+delta_x + x  >= 0 && i+delta_x + x  < (int)global_map->info.width &&
-	 j+delta_y + y  >= 0 && j+delta_y + y  < (int)global_map->info.height) {
-    int mycell = i + delta_x + x +(j + delta_y + y)*(int)global_map->info.width;
-    int theircell = i + j*(int)their_map.info.width;
-	if((int)global_map->data[mycell] == UNKNOWN || ( local && (int)their_map.data[theircell] != UNKNOWN && (int)global_map->data[mycell] != (int)their_map.data[theircell] ))
-    {
-        //ROS_INFO("merging...");
-	  global_map->data[mycell] = their_map.data[theircell];
-    }
-    }
+        if(i+delta_x + x  >= 0 && i+delta_x + x  < (int)global_map.info.width &&
+	        j+delta_y + y  >= 0 && j+delta_y + y  < (int)global_map.info.height) {
+            int mycell = i + delta_x + x +(j + delta_y + y)*(int)global_map.info.width;
+            int theircell = i + j*(int)their_map.info.width;
+            if((int)global_map.data[mycell] == UNKNOWN || ( furious && (int)their_map.data[theircell] != UNKNOWN && (int)global_map.data[mycell] != (int)their_map.data[theircell] ))
+            {
+                //ROS_INFO("merging...");
+            global_map.data[mycell] = their_map.data[theircell];
+            }
+        }
     }
   }
 }
-
-void merge_map( geometry_msgs::Pose p, int x, int y,  nav_msgs::OccupancyGrid msg, bool local)
+void mege_pipeline(bool furious)
 {
-    if(!global_map)
-    {
-        ROS_INFO("Global map not found, wait for local map");
+    geometry_msgs::Pose delta;
+    if ( !local_map ) {
+        ROS_INFO("Local map not found, wait for it");
         return;
-    
     }
-    else
-    {
+    std::map<std::string,  multi_master_bridge::MapData>::iterator it;
+    geometry_msgs::Pose offset;
+    getRelativePose(global_map.info.origin, local_map->info.origin, offset, local_map->info.resolution);
+    offset.position.x = abs(offset.position.x);
+    offset.position.y = abs(offset.position.y);
 
-        // merge two map here
-        geometry_msgs::Pose delta;
+    // fill global map with local content
+    std::fill(global_map.data.begin(), global_map.data.end(), -1);
+    for(int i= 0;i < local_map->info.width ; i++)
+        for(int j = 0; j < local_map->info.height; j++)
+            if(local_map->data[i + j*local_map->info.width] != -1)
+                global_map.data[(offset.position.y +  j)*global_map.info.width + offset.position.x + i] = local_map->data[i + j*local_map->info.width];
+        
+
+    //global_map.reset(new nav_msgs::OccupancyGrid(*local_map));
+    for ( it = pipeline.begin(); it != pipeline.end(); it++ )
+    {
+        geometry_msgs::Pose p;
+        p.position = it->second.position;
+        ROS_INFO("mergin map of %s with init pose (%f,%f,%f)",it->first.c_str(), it->second.position.x,it->second.position.y,it->second.position.z);
         ROS_INFO("Get relative position");
-        getRelativePose(p,my_pose, delta,global_map->info.resolution);
+        getRelativePose(p,my_pose, delta,global_map.info.resolution);
+        ROS_INFO("Get map offset");
+        getRelativePose(global_map.info.origin,it->second.map.info.origin,offset,global_map.info.resolution);
+        offset.position.x = abs(offset.position.x);
+        offset.position.y = abs(offset.position.y);
         ROS_INFO("merging");
-        greedyMerging(round(delta.position.x), round(delta.position.y),x,y, msg,local);
-         //global_map->header.frame_id = "map";
-        ros::Time now = ros::Time::now();
-        global_map->info.map_load_time = now;
-        global_map->header.stamp = now;
-        ROS_INFO("Publishing global map");
-        global_map_pub.publish(*global_map);
+        greedyMerging(round(delta.position.x), round(delta.position.y),offset.position.x,offset.position.y, it->second.map,furious);
+
     }
+    ros::Time now = ros::Time::now();
+    global_map.info.map_load_time = now;
+    global_map.header.stamp = now;
+    ROS_INFO("Publishing global map");
+    global_map_pub.publish(global_map);
 }
-void merge_local_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+/*
+int get_visible_zone(multi_master_bridge::MapData* data,const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-
-    ROS_INFO("Merging local map");
-    // first find map update
-    if(!global_map)
-    {
-        ROS_INFO("Global map not found, create it");
-        /*global_map.reset(new nav_msgs::OccupancyGrid(*msg));
-        // publish map update as the entire map
-        global_map_pub.publish(*global_map);*/
-
-        global_map.reset(new nav_msgs::OccupancyGrid());
-        global_map->header = msg->header;
-        global_map->info = msg->info;
-        global_map->data.resize(msg->info.width*msg->info.height,UNKNOWN);
-
-       // return;
-    }
-    // NOTE: The two maps has a same dimension
     int x = -1, y = -1,x1 = -1, y1 = -1, i,j;
     // find the bounding box for the updated zone
-    for(i = 0; i <global_map->info.width;i++)
-        for(j = 0; j < global_map->info.height;j++)
+    for(i = 0; i <msg->info.width;i++)
+        for(j = 0; j < msg->info.height;j++)
         {
-            int8_t cell = msg->data[i+j*msg->info.width];
-            if(global_map->data[i+j* global_map->info.width] != cell && cell != UNKNOWN )
+            if(msg->data[i+j* msg->info.width]  != UNKNOWN )
             {
                 x = i;
                 goto findy;
@@ -121,11 +120,10 @@ void merge_local_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
         }
     findy:
     if(x == -1) goto end;
-    for(i = 0; i <global_map->info.height;i++)
-        for(j = 0; j < global_map->info.width;j++)
+    for(i = 0; i <msg->info.height;i++)
+        for(j = 0; j < msg->info.width;j++)
         {
-            int8_t cell = msg->data[j+i*msg->info.width];
-            if(global_map->data[j+i* global_map->info.width] != cell && cell != UNKNOWN)
+            if(msg->data[j+i* msg->info.width] != UNKNOWN)
             {
                 y = i;
                 goto findw;
@@ -133,22 +131,20 @@ void merge_local_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
         }
     
     findw:
-    for(i = global_map->info.width -1; i>=0;i--)
-        for(j = 0; j < global_map->info.height;j++)
+    for(i = msg->info.width -1; i>=0;i--)
+        for(j = 0; j < msg->info.height;j++)
         {
-            int8_t cell = msg->data[i+j*msg->info.width];
-            if(global_map->data[i+j* global_map->info.width] != cell && cell != UNKNOWN)
+            if(msg->data[i+j* msg->info.width]  != UNKNOWN)
             {
                 x1 = i;
                 goto findh;
             }
         }
     findh:
-     for(i = global_map->info.height - 1; i >= 0;i--)
-        for(j = 0; j < global_map->info.width;j++)
+     for(i = msg->info.height - 1; i >= 0;i--)
+        for(j = 0; j < msg->info.width;j++)
         {
-            int8_t cell = msg->data[j+i*msg->info.width];
-            if(global_map->data[j+i* global_map->info.width] != cell && cell != UNKNOWN)
+            if(msg->data[j+i* msg->info.width] != UNKNOWN)
             {
                 y1 = i;
                 goto end;
@@ -157,73 +153,91 @@ void merge_local_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
     end:
     if(x == -1 || y == -1 || x1 == -1 || y1 == -1)
     {
-        ROS_INFO("no update found");
-        return;
+        ROS_INFO("No update found");
+        return 0;
     }
-    // create new map data with the update zone
-    multi_master_bridge::MapData update;
-    update.x = x;
-    update.y = y;
+    // new map data
     int w = x1 - x;
     int h = y1 - y;
-    update.map.header = msg->header;
-    update.map.info = msg->info;
-    update.map.info.width = w;
-    update.map.info.height = h;
-    update.position = my_pose.position;
-    ROS_INFO("update zone (%d,%d) (%d,%d)",x, y,w,h);
-    // new map data
-    update.map.data.resize(w*h,UNKNOWN);
+    data->x = x;
+    data->y = y;
+    data->map.header = msg->header;
+    data->map.info = msg->info;
+    data->map.info.width = w;
+    data->map.info.height = h;
+     ROS_INFO("update zone (%d,%d) (%d,%d)",x, y,w,h);
+    data->map.data.resize(w*h,UNKNOWN);
     // copy the update zone to the update map
     for(i=0; i < w;i++)
         for(j = 0; j < h; j++)
-            update.map.data[i+j*w] = msg->data[ x + i + (j+y)*msg->info.width ];
-    // publish the update
-    map_update_pub.publish(update);
-    // merge go here
-    ROS_INFO("greedy_local_merge: %d",(int)greedy_local_merge);
-    merge_map(my_pose,update.x, update.y, update.map, greedy_local_merge);
-    ROS_INFO("Merged");
+            data->map.data[i+j*w] = msg->data[ x + i + (j+y)*msg->info.width ];
+    return 1;
 }
-void merge_their_map(const multi_master_bridge::MapData::ConstPtr& msg)
+*/
+void register_local_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-    geometry_msgs::Pose p;
-    p.position = msg->position;
-    ROS_INFO("Merging map from another robot: %s", msg->ip.c_str());
-    ROS_INFO("Their init pose is [%f,%f,%f]\n",p.position.x,p.position.y, p.position.z);
-    ROS_INFO("update zone (%d,%d) (%d,%d)",msg->x, msg->y,msg->map.info.width,msg->map.info.height);
-    other_map_pub.publish(msg->map);
-    merge_map(p,msg->x, msg->y,msg->map, false);
+    ROS_INFO("Local map found");
+    multi_master_bridge::MapData visibledata;
+    visibledata.position = my_pose.position;
+    visibledata.x = 0;
+    visibledata.y = 0;
+    visibledata.map = *msg;
+    map_update_pub.publish(visibledata);
+    local_map.reset(new nav_msgs::OccupancyGrid(*msg)); 
 }
+
+void register_neighbor_map(const multi_master_bridge::MapData::ConstPtr& msg)
+{
+    ROS_INFO("Registering neighbor map");
+    pipeline[msg->ip] = *msg;
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "greedy_merging");
 	ros::NodeHandle n("~");
+    
     n.param<std::string>("other_map",other_map_, "/other_map");
     n.param<std::string>("my_map",my_map_, "/map");
     n.param<std::string>("map_update_topic",map_update_, "/map_update");
-    n.param<std::string>("map_other_topic",map_other_, "/other_map");
     n.param<std::string>("merged_map_topic",merged_map_topic, "/global_map");
-    n.param<bool>("greedy_local_merge",greedy_local_merge, true);
+    n.param<bool>("furious_merge",furious_merge, false);
     n.param<double>("init_z",my_pose.position.z, 0.0);
 	n.param<double>("init_x",my_pose.position.x, 0.0);
 	n.param<double>("init_y",my_pose.position.y, 0.0);
+    n.param<double>("map_width_m",map_width_m_, 10.0);
+    n.param<double>("map_height_m",map_height_m_, 10.0);
+    n.param<double>("map_resolution",map_resolution_, 0.05);
+    int w =  round((map_width_m_ )/map_resolution_);
+    int h = round((map_height_m_ )/map_resolution_);
+    global_map.data.resize(w*h,-1);
+    global_map.info.width = w;
+    global_map.info.height = h;
+    global_map.info.resolution = map_resolution_;
+    global_map.info.origin.position.x = -map_width_m_/2.0;
+    global_map.info.origin.position.y = -map_height_m_/2.0;
+    global_map.info.origin.position.z = 0.0;
+    global_map.info.origin.orientation.x = 0.0;
+    global_map.info.origin.orientation.y = 0.0;
+    global_map.info.origin.orientation.z = 0.0;
+    global_map.info.origin.orientation.w = 1.0;
     // subscribe to this map
     ROS_INFO("My initial position is [%f,%f,%f]\n",my_pose.position.x,my_pose.position.y, my_pose.position.z);
     ROS_INFO("My Map topic is %s",my_map_.c_str());
     ROS_INFO("Other map topic is %s", other_map_.c_str());
     ROS_INFO("Global map topic is %s", merged_map_topic.c_str());
-    ros::Subscriber sub = n.subscribe<multi_master_bridge::MapData>(other_map_, 50,&merge_their_map);
-	ros::Subscriber sub1 = n.subscribe<nav_msgs::OccupancyGrid>(my_map_, 50,&merge_local_map);
-    global_map = nullptr;
+    ROS_INFO("Furios merge is  %d", furious_merge );
+    ros::Subscriber sub = n.subscribe<multi_master_bridge::MapData>(other_map_, 50,&register_neighbor_map);
+	ros::Subscriber sub1 = n.subscribe<nav_msgs::OccupancyGrid>(my_map_, 50,&register_local_map);
+    local_map = nullptr;
     // publisher register
     global_map_pub = n.advertise<nav_msgs::OccupancyGrid>(merged_map_topic, 50, true);
-    other_map_pub = n.advertise<nav_msgs::OccupancyGrid>(map_other_, 50, true);
     map_update_pub = n.advertise<multi_master_bridge::MapData>(map_update_, 50, true);
-    ros::Rate r(5);
+    ros::Rate r(1);
     while(ros::ok())
     {
         ros::spinOnce();
+        mege_pipeline(furious_merge);
         r.sleep();
     }
 }
